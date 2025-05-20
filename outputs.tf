de733229d1eacd5d2273ab5159f89054b0ff4947
Robin Norwood <rnorwood@hashicorp.com>
2025-05-20 16:02:15 -0500

    Remove EC2 instance

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "instance_hostname" {
-  description = "Public DNS name of the EC2 instance."
-  value       = aws_instance.app_server.public_dns
-}
+# output "instance_hostname" {
+#   description = "Public DNS name of the EC2 instance."
+#   value       = aws_instance.app_server.public_dns
+# }
