e42aee1231124c12c77883117852b34bcccc3b46
Robin Norwood <rnorwood@hashicorp.com>
2025-05-20 16:46:59 -0500

    Connect to HCP Terraform

--- main.tf
+++ main.tf
@@ -29,14 +29,14 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-# resource "aws_instance" "app_server" {
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = var.instance_type
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
 
-#   vpc_security_group_ids = [module.vpc.default_security_group_id]
-#   subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
 
-#   tags = {
-#     Name = var.instance_name
-#   }
-# }
+  tags = {
+    Name = var.instance_name
+  }
+}
--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,13 @@
 terraform {
+  cloud {
+    organization = "your-organization-name"
+
+    workspaces {
+      project = "Learn Terraform"
+      name    = "learn-terraform-aws-get-started"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
